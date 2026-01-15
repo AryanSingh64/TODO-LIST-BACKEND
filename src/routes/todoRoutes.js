08b729833a4edf8b845c2c284121827f34864fa4
Aryan Singh <93329923+Warlord64@users.noreply.github.com>
2026-01-15 14:55:46 +0530

    Completed without DataBase

--- src/routes/todoRoutes.js
+++ src/routes/todoRoutes.js
@@ -13,18 +13,34 @@ router.get('/',(req,res)=>{
 
 //Create a new todo
 router.post('/',(req,res)=>{
-    
+      const {task} = req.body
+      const insertTodos = db.prepare(`INSERT INTO todos (user_id, task) VALUES (?, ?)`)
+      const result = insertTodos.run(req.userId, task)
+
+      res.json({id: result.lastInsertRowid, task , completed: 0})
 })
 
 //update a todo -- we check id in db before change
 router.put('/:id',(req,res)=>{
+    const { completed } = req.body
+    const { id } = req.params
+    const { page } = req.query
 
+    const updateTodo = db.prepare('UPDATE todos SET completed = ? WHERE id = ?')
+
+    updateTodo.run(completed,id)
+
+    res.json({message: "Todo Completed"})
 })
 
 
 //DELETE
 router.delete('/:id',(req,res)=>{
-
+    const {id} = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
+    deleteTodo.run(id,userId)
+    res.send({message:"Todo DELETED"})
 })
 
 export default router
